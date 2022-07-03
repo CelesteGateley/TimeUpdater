8a2c2dee3b256ba4142578c7d7767e47383d1ddb
Celeste Gateley <celeste.gateley@gmail.com>
2022-07-03 12:21:25 +0100

    Initial commit

---
+++ src/main/java/xyz/fluxinc/timeupdater/TimeUpdater.java
@@ -0,0 +1,17 @@
+package xyz.fluxinc.timeupdater;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TimeUpdater extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
